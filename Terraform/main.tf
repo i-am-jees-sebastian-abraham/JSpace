70c26300a32e701f58005995cfb9494e0c6ab644
jeessebastian <jeeskonnackal@gmail.com>
2026-06-22 15:49:09 +0530

    Updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku				  = "Basic"
+  sku				  = "Standard"
 }
 
 # Create Network Security Group and rule
